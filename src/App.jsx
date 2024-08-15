21687e70b8a1ced44fca58cac99832af36685702
Jenny Stark <jenny.stark1017@gmail.com>
2024-08-15 11:12:11 +0200

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import Header from './components/header/header';
 import './App.css'
 
 function App() {
@@ -7,7 +6,7 @@ function App() {
 
   return (
     <>
-      
+     <Header /> 
     </>
   )
 }
---
+++ src/components/header/header.jsx
@@ -0,0 +1,16 @@
+import './header.css';
+
+function Header() {
+
+    return (
+        
+        <div id='header'>
+
+<h1>Välkommen till Chatify</h1>
+        </div>
+          
+        
+      )
+} 
+
+export default Header;
