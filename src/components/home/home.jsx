de2e44287e82d4f3996967c40b61e02540294594
Jenny Stark <jenny.stark1017@gmail.com>
2024-08-16 09:53:26 +0200

    Home done

---
+++ src/components/home/home.jsx
@@ -0,0 +1,14 @@
+import './home.css';
+
+function Home () {
+
+    return (
+        <>
+        <h2>Välkommen till Chatify!</h2>
+        <p>Börja chatta genom att logga in. Om du inte har ett konto börjar du med att registrera dig</p>
+        
+    </>
+    );
+};
+
+export default Home;
