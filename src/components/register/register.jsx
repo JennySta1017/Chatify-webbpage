e6a78b9ccc580e87b9e554404526b377dcd06a6a
Jenny Stark <jenny.stark1017@gmail.com>
2024-08-16 16:14:11 +0200

    Utv form klar

--- src/components/register/register.jsx
+++ src/components/register/register.jsx
@@ -4,21 +4,61 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
 function Register() {
+    const [newUser, setnewUser] = useState([]);
     const [userName, setuserName] = useState("");
-    const [password, setpassword] = useState(""); 
+    const [password, setpassword] = useState("");
+    const [email, setemail] = useState("");
+
+    const HandleSubmit = async (event, userName, password, email) => {
+      event.preventDefault();
+
+      const userObj = {
+        username: userName,
+        password: password,
+        email: email,
+        
+      };
+
+      if(!response.ok) {
+        alert("Något gick fel");
+
+      }
+
+      const response = await fetch('https://chatify-api.up.railway.app/auth/register', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(userObj),
+      });
+
+      return (
+        <>
+        setnewUser([...newUser, userObj]);
+        navigate("/login");
+        </>
+      )
+
+    }
+
     return (
         <>
         <h2>Registrera dig nedan</h2>
          <Form id="registerform">
-          <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Group className="mb-3" controlId="formBasicUsername">
           <Form.Label>Ange nytt användarnamn:</Form.Label>
-          <Form.Control className="input" type="text" placeholder="Användarnamn" value={userName}/>
+          <Form.Control className="input" type="text" placeholder="Användarnamn" value={userName} onChange={(e) => setuserName(e.target.value)}/>
           </Form.Group>
           
           <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Ange nytt lösenord:</Form.Label>
-          <Form.Control className="input" type="password" placeholder="Lösenord" value={password}/>
+          <Form.Control className="input" type="password" placeholder="Lösenord" value={password} onChange={(e) => setpassword(e.target.value)}/>
           </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>E-post address:</Form.Label>
+        <Form.Control className="input" type="email" placeholder="E-post" value={email} onChange={(e) => setemail(e.target.value)} />
+        </Form.Group>
          
           <Button id="formbutton" variant="dark" type="submit">
             Spara
@@ -30,3 +70,4 @@ function Register() {
 };
 
 export default Register;
+
