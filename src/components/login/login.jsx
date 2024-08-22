f2f2e9ca0995010e733ec80734928191537b0089
Jenny Stark <jenny.stark1017@gmail.com>
2024-08-22 11:41:21 +0200

    Login works

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -3,23 +3,29 @@ import './login.css';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
-const Login = () => {
+const Login = ({
+  handleLogin,
+  userName,
+  setUserName,
+  password,
+  setPassword,
+}) => {
 
     return (
     
     <Form id='loginform'>
     <Form.Group className="mb-3" controlId="formBasicUser">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control className="input" type="email" placeholder="Skriv in användarnamn" />
+        <Form.Label>Användarnamn</Form.Label>
+        <Form.Control className="input" type="text" placeholder="Skriv in användarnamn" value={userName} onChange={(e) => setUserName(e.target.value)}/>
 
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicPass">
         <Form.Label>Password</Form.Label>
-        <Form.Control className="input" type="password" placeholder="Skriv in lösenord" />
+        <Form.Control className="input" type="password" placeholder="Skriv in lösenord" value={password} onChange={(e) => setPassword(e.target.value)}/>
       </Form.Group>
       
-      <Button id= "loginbutton" variant="dark" type="submit">
+      <Button id= "loginbutton" variant="dark" type="button" onClick={handleLogin}>
         Logga in
       </Button>
     </Form>
