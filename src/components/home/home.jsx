917fde8a1bb95823fa830d46df32b3285b0de9af
Jenny Stark <jenny.stark1017@gmail.com>
2024-08-28 10:59:58 +0200

    snyggar till kod

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -3,11 +3,11 @@ import './home.css';
 function Home () {
 
     return (
-        <>
-        <div id='home-box'>
+    <>
+    <div id='home-box'>
         <h2>Välkommen till Chatify!</h2>
         <p>Börja chatta genom att logga in. Om du inte har ett konto börjar du med att registrera dig</p>
-        </div>
+    </div>
     </>
     );
 };
--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,5 +1,4 @@
-import './login.css';
-
+import './Login.css';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
@@ -9,33 +8,41 @@ const Login = ({
   setUserName,
   password,
   setPassword,
-}) => {
+    }) => {
 
     return (
       <>
       <div id='login-text'>
-    <h1>Logga in på ditt konto</h1>
-    
-    </div>
-    <Form id='loginform'>
-    <Form.Group className="mb-3" controlId="formBasicUser">
+        <h1>Logga in på ditt konto</h1>
+      </div>
+      <Form id='loginform'>
+        <Form.Group className="mb-3" controlId="formBasicUser">
         <Form.Label>Användarnamn</Form.Label>
-        <Form.Control className="input" type="text" placeholder="Skriv in användarnamn" value={userName} onChange={(e) => setUserName(e.target.value)}/>
+        <Form.Control 
+          className="input" 
+          type="text" 
+          placeholder="Skriv in användarnamn" 
+          value={userName} 
+          onChange={(e) => setUserName(e.target.value)}/>
+        </Form.Group>
 
-      </Form.Group>
-
-      <Form.Group className="mb-3" controlId="formBasicPass">
+        <Form.Group className="mb-3" controlId="formBasicPass">
         <Form.Label>Password</Form.Label>
-        <Form.Control className="input" type="password" placeholder="Skriv in lösenord" value={password} onChange={(e) => setPassword(e.target.value)}/>
-      </Form.Group>
+        <Form.Control 
+          className="input"   
+          type="password" 
+          placeholder="Skriv in lösenord" 
+          value={password} 
+          onChange={(e) => setPassword(e.target.value)}/>
+        </Form.Group>
       
-      <Button id= "loginbutton" variant="dark" type="button" onClick={handleLogin}>
-        Logga in
-      </Button>
-    </Form>
-    </>
+        <Button id= "loginbutton" variant="dark" type="button" onClick={handleLogin}>
+          Logga in
+        </Button>
+      </Form>
+      </>
     
-    )
+    );
 };
 
 export default Login;
